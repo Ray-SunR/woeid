f2fa380107e7853139902b465d3ccae1744fb40c
Renchen <sunrenchen@gmail.com>
2016-07-16 14:33:16 -0700

    Bug fixes. Added apis for setting api properties such as format, lang, count, start, baseurl, etc.

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
 __author__ = 'Renchen'
+from distutils.core import setup
+
+setup(
+    name='woeid',
+    version='0.1.1',
+    author='Renchen Sun',
+    author_email='sunrenchen@gmail.com',
+    packages=['woeid', 'woeid.test'],
+    url='https://github.com/Ray-SunR/woeid',
+    download_url="https://github.com/Ray-SunR/woeid/archive/0.1.zip",
+    license=open('LICENSE.txt').read(),
+    description='A python REST apis for getting Yahoo GeoPlanet web services (https://developer.yahoo.com/geo/geoplanet/guide/)',
+    long_description=open('README.md').read(),
+    keywords=['woeid', 'geoplanet', 'yahoo']
+)
